4a2994c6fc274c350e83e9f09dddea3433784447
Filip <143887268+FilipMast@users.noreply.github.com>
2023-12-18 14:17:45 +0100

    Update Sprint_5.js

--- Sprint_5.js
+++ Sprint_5.js
@@ -163,6 +163,106 @@ function getColorByNumber(number) {
 
 getColorByNumber(5);
 
+// // ZAD 1 //
+
+// Dane wejściowe //
+// const people = [
+//   {
+//     firstName: "Alicja",
+//     lastName: "Kowalska",
+//   },
+//   {
+//     firstName: "Jan",
+//     lastName: "Nowak",
+//   }
+//    {
+//     firstName: "Halina",
+//     lastName: "Malina",
+//   }
+// ];
+
+// Napisz funkcję, która przetwarza każdą osobę w tablicy people w następujący sposób:
+// a) Dla każdego imienia, weź ostatnie 3 litery, odwróć ich kolejność i zapisz do zmiennej.
+// b) Dla każdego nazwiska, weź pierwsze 3 litery, zamień miejscami pierwszą i ostatnią literę, i dołącz do zmiennej utworzonej w punkcie a).
+// c) Zmień wielkość liter w taki sposób, aby ostateczny pseudonim (nickname) zaczynał się wielką literą, a reszta liter była mała.
+// d) Jeśli liczba znaków w imieniu lub nazwisku jest mniejsza niż 2, pseudonim będzie odpowiednio krótszy.
+
+// // ZAD 2 //
+
+// Dane wejściowe
+// const people = [
+//   {
+//     firstName: "Alicja",
+//     lastName: "Kowalska",
+//     nickname: "Ajcwok"
+//   },
+//   {
+//     firstName: "Jan",
+//     lastName: "Nowak",
+//     nickname:"Najwon",
+//   },
+//   {
+//     firstName: "Halina",
+//     lastName: "Malina",
+//     nickname:"Anilam",
+//   }
+// ];
+
+// Rozszerz funkcję z poprzedniego zadania:
+// Dodaj pole age, które jest wyliczane na podstawie sumy liter w imieniu i nazwisku. Jeżeli ilość liter w imieniu i
+// nazwisku jest parzysta to wiek będzie będzie wyliczany, na postawie długości kluczy znajdujących się w obiekcie pomniejszone o długość imienia.
+
+// // Zad 3 //
+
+// Dane Wejściowe
+// const people = [
+//   {
+//     firstName: "Alicja",
+//     lastName: "Kowalska",
+//     nickname: "Ajcwok"
+//     age:19
+//   },
+//   {
+//     firstName: "Jan",
+//     lastName: "Nowak",
+//     nickname:"Najwon",
+//     age:22
+//   },
+//   {
+//     firstName: "Waldemar",
+//     lastName: "Malina",
+//     nickname:"Ramlam",
+//     age:17
+//   }
+// ];
+
+//     a) Do każdego obiektu dodaj funkcję introduceYourself, która za pomocą słówka this wyświetli w konsoli tekst powitalny.
+//     Dla powyższego przykładu tekst powinien wyglądać w następujący sposób:
+//     "Cześć jestem Alicja Kowalska, ale w szkole mówią na mnie [Ajcwok]."
+//     b) za pomocą pętli forEach, wywołaj funkcję powitalną dla każdego elementu tablicy. W rezultacie na ekranie powinien
+//     pojawić się tekst powitalny dla każdej osoby w tablicy
+
+// ZAD 4.
+
+//  // Dane wejściowe //
+//  [{
+//     firstName: "Alicja",
+//     lastName: "Kowalska",
+//     nickname: "Ajcwok",
+//     age:19,
+//     introduceYourself: // funkcja //
+//     }
+//     itd ... ]
+
+// const colors = ["red", "green", "yellow", "blue", "pink", "orange"];
+
+// Napisz funkcję, która :
+//     a) przyjmie parametr typu number z zakresu 1 - 30
+//     b) funkcja ma przyjmować jeden parametr typu number z zakresu 1 - 30
+//     c) jeżeli podany parametr jest poza zakresem, powinien wyświetlić się odpowiedni komunikat
+//         - podałeś za małą liczbę, liczba nie może być mniejsza niż 1
+//         - podałeś za dużą liczbę, liczba nie może być większa niż 30
+//     d) w przypadku wywołania funkcji bez parametru, powinniśmy ustawić domyślną wartość na 5
 //     e) funkcja powinna zsumować pary klucz + wartość trzech pierwszych pozycji w obiekcie (użyj do tego Object.entries)
 //     odjąć od tej sumy liczbę wprowadzoną w parametrze, a następnie za pomocą działania modulo (%) względem długości tablicy kolorów
 //     wyznaczyć index
